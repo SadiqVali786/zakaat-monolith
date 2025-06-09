1cba16227a7ee4ce5b0b1b8af6e5039934613001
SadiqVali786 <rebirth4vali@gmail.com>
2025-06-09 23:34:52 +0530

    [chore] changing the text on the hero section

--- src/app/(landing-page)/_components/hero/index.tsx
+++ src/app/(landing-page)/_components/hero/index.tsx
@@ -6,7 +6,7 @@ export const Hero = () => {
     <SectionTemplate
       pillText="✨ Find Deserving Zakaat Recipients Here"
       headingPart1="Maximize Your Zakaat Impact, "
-      headingPart2="Support the Deserving Zakaat Applicants"
+      headingPart2="Support the Deserving Applicants"
       infoLine="Fulfill your duty of Zakat with purpose, backed by the power of Artificial Intelligence. Our platform ensures your contributions reach truly deserving Applicants by safeguarding against fraudulent applications. Connect with those genuinely in need."
       headingFontClamp="text-35-to-65-with-768-to-1440"
       sectionImage={iPhoneHero}
